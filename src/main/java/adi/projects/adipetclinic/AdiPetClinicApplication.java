41ef9ed90347732a5868d2fed7c10df35c676534
Buciuman Adrian <adieuadi@gmail.com>
2020-11-02 15:50:45 +0200

    Initial Project Add

---
+++ src/main/java/adi/projects/adipetclinic/AdiPetClinicApplication.java
@@ -0,0 +1,13 @@
+package adi.projects.adipetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdiPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdiPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/adi/projects/adipetclinic/AdiPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package adi.projects.adipetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdiPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
