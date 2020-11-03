53347b0fe2eb584cf5f2a8bc26a0abe30cc997fb
Buciuman Adrian <adieuadi@gmail.com>
2020-11-03 22:13:13 +0200

    implement Map based Services. closes #6

---
+++ pet-clinic-data/src/main/java/adi/projects/adipetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package adi.projects.adipetclinic.services.map;
+
+import adi.projects.adipetclinic.model.Pet;
+import adi.projects.adipetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
