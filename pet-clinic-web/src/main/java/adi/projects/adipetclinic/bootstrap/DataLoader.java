2c4d66ee1e0bd3c64ce23eb3bcd303e7eeda051a
Buciuman Adrian <adieuadi@gmail.com>
2020-11-04 11:22:19 +0200

    Added Owners list and updated controller . closes #16

--- pet-clinic-web/src/main/java/adi/projects/adipetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/adi/projects/adipetclinic/bootstrap/DataLoader.java
@@ -31,9 +31,9 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("Michael2");
-        owner1.setLastName("John2");
+        owner2.setId(2L);
+        owner2.setFirstName("Michael2");
+        owner2.setLastName("John2");
 
         ownerService.save(owner2);
 
